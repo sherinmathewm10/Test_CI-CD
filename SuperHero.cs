a58d8f5aff64703525ad2620efaf0d19d1658202
sherinmathewm10 <108885284+sherinmathewm10@users.noreply.github.com>
2023-07-12 11:43:48 +0100

    Add files via upload

---
+++ SuperHero.cs
@@ -0,0 +1,11 @@
+﻿namespace SuperHeroWebAPI
+{
+    public class SuperHero
+    {
+        public int id { get; set; }
+        public string name { get; set; } = string.Empty;
+        public string firstName { get; set; } = string.Empty;
+        public string lastName { get; set; }= string.Empty;
+        public string place { get; set; } = string.Empty;
+    }
+}
